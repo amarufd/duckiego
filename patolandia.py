4e85bd9559d91da11f024012f41b28f741112394
Amaru <amarufd@gmail.com>
2019-12-31 14:05:49 -0300

    se hacen correcciones v3

--- patolandia.py
+++ patolandia.py
@@ -1,121 +1,41 @@
 #!/usr/bin/env python
 # manual
 
-"""
-This script allows you to manually control the simulator or Duckiebot
-using the keyboard arrows.
-"""
-
 import sys
 import argparse
 import pyglet
-from pyglet.window import key
+import cv2
 import numpy as np
 import gym
 import gym_duckietown
+import math
+import transformations as tf
+from pyglet.window import key
 from gym_duckietown.envs import DuckietownEnv
 from gym_duckietown.wrappers import UndistortWrapper
-
-####
 from PIL import Image
-import cv2
-import math
 from apriltag import Detector
-import transformations as tf
-
-#calculo direccion
-#import LineRoadsDetection as LR
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-def make_points(line, altura, ancho, altura_y, offset_y):
-    inclinacion, interseccion = line
-    y1=int(altura/2+altura_y+offset_y) #abajo
-    y2=int(altura/2-altura_y+offset_y) #arriba
-
-    # encuadrando las coordenadas con el cuadro
-    x1 = max(-ancho, min(2 * ancho, int((y1 - interseccion) / inclinacion)))
-    x2 = max(-ancho, min(2 * ancho, int((y2 - interseccion) / inclinacion)))
-    return [[x1, y1, x2, y2]]
-
-def calcula_direccion(cv_img, erode_it, dilate_it, altura_y, offset_y, boundary, lower_yellow, upper_yellow, lower_white, upper_white):
-    altura, ancho, _= cv_img.shape
-    # Se transforma la imagen a HSV
-    hsv_img=cv2.cvtColor(cv_img, cv2.COLOR_BGR2HSV)
-    # Segmentacion para los colores de borde de la calle (blanco y amarillo)
-    mascara_amarilla = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
-    mascara_blanca =  cv2.inRange(hsv_img, lower_white, upper_white)
-
-    #Se unen ambas máscaras, así se tiene una segmentación de blancos y amarillos
-    mascara=cv2.bitwise_or(mascara_blanca,mascara_amarilla)
-    #Inicializamos la detección
-    lds=DeteccionLineas(cv_img, altura_y, offset_y, boundary)
-    #Asignamos un espacio de interés de la máscara
-    mascara=lds.region_evaluar(mascara)
-    #Realizamos erosión y dilatación
-    kernel = np.ones((5,5),np.uint8)
-    erosion = cv2.erode(mascara, kernel, iterations=erode_it)
-    dilated = cv2.dilate(erosion, kernel, iterations=dilate_it)
-    #obtenemos una imágen ajustada a la máscara
-    image_out = cv2.bitwise_and(cv_img, cv_img, mask=dilated) # Aplicar mascara
-
-    #Se detectan los bordes con el método Canny de OPENCV
-    esquinas=cv2.Canny(mascara,200,400)
-
-    #Se usa el procedimiento segmentos_lineas para detectar las líneas en la imágen
-    segmentos_lineas=lds.detectar_segmentos_lineas(esquinas)
-    #Se calcula una línea que es el resultado de un promedio ponderado de todas las líneas
-    #detectadas
-    lane_lines = lds.promedio_interseccion_rectas(segmentos_lineas)
-    #Como output se muestra la imágen
-    lane_lines_image = lds.display_lines(lane_lines)
-
-    if len(lane_lines)==0:
-        x_offset=0
-        y_offset = int(altura/2-altura_y+offset_y)
-    elif len(lane_lines)==1:
-        x1, _, x2, _ = lane_lines[0][0]
-        x_offset = x2 - x1
-        y_offset = int(altura/2-altura_y+offset_y)
-    elif len(lane_lines)==2:
-        _, _, left_x2, _=lane_lines[0][0]
-        _, _, right_x2, _=lane_lines[1][0]
-        mid=int(ancho/2)
-        x_offset=(left_x2+right_x2)/2-mid
-        y_offset=int(altura/2-altura_y+offset_y)
-
-
-    direccion_angulo = math.atan(x_offset / y_offset) #radianes
-    if x_offset==0:
-        direccion_angulo=+math.pi*1/12
-
-    cv2.imshow("maskara", mascara)
-    cv2.imshow("image salida",image_out)
-    cv2.imshow("lane lines", lane_lines_image)
-
-    return direccion_angulo
-
 
 class DeteccionLineas():
-    def __init__(self, frame, altura_y, offset_y, boundary):
+    def __init__(self, frame, altura_y, correccion_y, limites):
         self.frame=frame
         self.altura, self.ancho, _= frame.shape
         self.altura_y=altura_y
-        self.offset_y=offset_y
-        self.limite=boundary
+        self.correccion_y=correccion_y
+        self.limite=limites
 
     # Regio que donde se evaluan las lineas que se seguiran
-    def region_evaluar(self, mascara):
-        region_a_recortar = [[0,int(self.altura/2+self.altura_y+self.offset_y)],[0,int(self.altura)],[int(self.ancho),int(self.altura)],[int(self.ancho),int(self.altura/2+self.altura_y+self.offset_y)]]
+    def regionEvaluar(self, mascara):
+        region_a_recortar = [[0,int(self.altura/2+self.altura_y+self.correccion_y)],[0,int(self.altura)],[int(self.ancho),int(self.altura)],[int(self.ancho),int(self.altura/2+self.altura_y+self.correccion_y)]]
         poligono_a_recortar = np.array([region_a_recortar],dtype=np.int32)
         cv2.fillPoly(mascara,poligono_a_recortar,0)
-        region_a_recortar = [[0,int(self.altura/2-self.altura_y+self.offset_y)],[0,0],[int(self.ancho),0],[int(self.ancho),int(self.altura/2-self.altura_y+self.offset_y)]]
+        region_a_recortar = [[0,int(self.altura/2-self.altura_y+self.correccion_y)],[0,0],[int(self.ancho),0],[int(self.ancho),int(self.altura/2-self.altura_y+self.correccion_y)]]
         poligono_a_recortar = np.array([region_a_recortar],dtype=np.int32)
         cv2.fillPoly(mascara,poligono_a_recortar,0)
         return mascara
 
     # Deteccion de los segmentos que ayudaran a la conduccion del pato por el centro
-    def detectar_segmentos_lineas(self, esquinas):
+    def detectarSegmentosLineas(self, esquinas):
         rho = 1
         angulo = np.pi / 180
         lim_menor = 20
@@ -124,7 +44,7 @@ class DeteccionLineas():
         return segmentos_lineas
 
     # Calcula el promedio de la interseccion de las rectas para el manejo del pato
-    def promedio_interseccion_rectas(self, segmentos_lineas):
+    def promedioInterseccionRectas(self, segmentos_lineas):
         lineas_seguir = []
         if segmentos_lineas is None:
             return lineas_seguir
@@ -134,8 +54,8 @@ class DeteccionLineas():
         ajuste_der = []
         ancho_der=[]
 
-        limite_izq = self.ancho * (1 - self.limite)  # la linea de la izquierda está por dentro de la variable boundary por la izquierda
-        limite_der = self.ancho * self.limite # la linea de la derecha está por dentro de la variable boundary por la derecha
+        limite_izq = self.ancho * (1 - self.limite)  # la linea de la izquierda está por dentro de la variable limites por la izquierda
+        limite_der = self.ancho * self.limite # la linea de la derecha está por dentro de la variable limites por la derecha
 
         for line_segment in segmentos_lineas:
             for x1, y1, x2, y2 in line_segment:
@@ -163,27 +83,24 @@ class DeteccionLineas():
 
         if len(ajuste_izq) > 0:
             ajuste_izq_average = np.average(ajuste_izq,weights=ancho_izq, axis=0)
-            lineas_seguir.append(make_points(ajuste_izq_average, self.altura, self.ancho, self.altura_y, self.offset_y))
+            lineas_seguir.append(puntos(ajuste_izq_average, self.altura, self.ancho, self.altura_y, self.correccion_y))
 
         if len(ajuste_der) > 0:
             ajuste_der_average = np.average(ajuste_der,weights=ancho_der, axis=0)
-            lineas_seguir.append(make_points(ajuste_der_average, self.altura, self.ancho, self.altura_y, self.offset_y))
+            lineas_seguir.append(puntos(ajuste_der_average, self.altura, self.ancho, self.altura_y, self.correccion_y))
 
         return lineas_seguir
     
 
-    def display_lines(self, lines, line_color=(0, 255, 0), line_ancho=2):
-        line_image = np.zeros_like(self.frame)
-        if lines is not None:
-            for line in lines:
-                for x1, y1, x2, y2 in line:
-                    cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_ancho)
-        line_image = cv2.addWeighted(self.frame, 0.8, line_image, 1, 1)
-        return line_image
+    def muestraLineas(self, lineas, linea_color=(0, 255, 0), linea_ancho=2):
+        lineas_imagen = np.zeros_like(self.frame)
+        if lineas is not None:
+            for linea in lineas:
+                for x1, y1, x2, y2 in linea:
+                    cv2.line(lineas_imagen, (x1, y1), (x2, y2), linea_color, linea_ancho)
+        lineas_imagen = cv2.addWeighted(self.frame, 0.8, lineas_imagen, 1, 1)
+        return lineas_imagen
 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-# from experiments.utils import save_img
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--env-name', default='Duckietown-udem1-v0') #Ingresamos el ambiente
@@ -212,6 +129,66 @@ else:
 env.reset()
 env.render()
 total_reward = 0
+
+def puntos(line, altura, ancho, altura_y, correccion_y):
+    inclinacion, interseccion = line
+    y1=int(altura/2+altura_y+correccion_y) #abajo
+    y2=int(altura/2-altura_y+correccion_y) #arriba
+
+    # encuadrando las coordenadas con el cuadro
+    x1 = max(-ancho, min(2 * ancho, int((y1 - interseccion) / inclinacion)))
+    x2 = max(-ancho, min(2 * ancho, int((y2 - interseccion) / inclinacion)))
+    return [[x1, y1, x2, y2]]
+
+def calcula_direccion(cv_img, erode_it, dilate_it, altura_y, correccion_y, limites, amarillo_claro, amarillo_oscuro, blanco_claro, blanco_oscuro):
+    
+    altura, ancho, _= cv_img.shape
+    imagen_hsv=cv2.cvtColor(cv_img, cv2.COLOR_BGR2HSV)
+
+    # Segmentacion para los colores de borde de la calle (blanco y amarillo) y se unen  las mascaras
+    mascara_amarilla = cv2.inRange(imagen_hsv, amarillo_claro, amarillo_oscuro)
+    mascara_blanca =  cv2.inRange(imagen_hsv, blanco_claro, blanco_oscuro)
+    mascara=cv2.bitwise_or(mascara_blanca,mascara_amarilla)
+
+    # Deteccion de lineas para la conduccion del pato
+    lineas_detectadas=DeteccionLineas(cv_img, altura_y, correccion_y, limites)
+    mascara=lineas_detectadas.regionEvaluar(mascara)
+    kernel = np.ones((5,5),np.uint8)
+    erosion = cv2.erode(mascara, kernel, iterations=erode_it)
+    dilated = cv2.dilate(erosion, kernel, iterations=dilate_it)
+    
+    esquinas=cv2.Canny(mascara,200,400)
+
+    segmentos_lineas=lineas_detectadas.detectarSegmentosLineas(esquinas)
+    lineas_calle = lineas_detectadas.promedioInterseccionRectas(segmentos_lineas)
+
+    if len(lineas_calle)==0:
+        x_correccion=0
+        y_correccion = int(altura/2-altura_y+correccion_y)
+    elif len(lineas_calle)==1:
+        x1, _, x2, _ = lineas_calle[0][0]
+        x_correccion = x2 - x1
+        y_correccion = int(altura/2-altura_y+correccion_y)
+    elif len(lineas_calle)==2:
+        _, _, left_x2, _=lineas_calle[0][0]
+        _, _, right_x2, _=lineas_calle[1][0]
+        mid=int(ancho/2)
+        x_correccion=(left_x2+right_x2)/2-mid
+        y_correccion=int(altura/2-altura_y+correccion_y)
+
+    direccion_angulo = math.atan(x_correccion / y_correccion)
+    if x_correccion==0:
+        direccion_angulo=+math.pi*1/12
+
+    
+    lineas_detectadas = lineas_detectadas.muestraLineas(lineas_calle)
+    imagen_salida = cv2.bitwise_and(cv_img, cv_img, mask=dilated)
+    cv2.imshow("mascara", mascara)
+    cv2.imshow("image salida",imagen_salida)
+    cv2.imshow("lane lineas", lineas_detectadas)
+
+    return direccion_angulo
+
 
 def _draw_pose(overlay, camera_params, tag_size, pose, z_sign=1):
 
@@ -307,22 +284,22 @@ while True:
     ### detectar lineas
     original = Image.fromarray(obs)
     cv_img = cv2.cvtColor(np.array(original), cv2.COLOR_RGB2BGR)
-    hsv_img = cv2.cvtColor(cv_img, cv2.COLOR_BGR2HSV)
+    imagen_hsv = cv2.cvtColor(cv_img, cv2.COLOR_BGR2HSV)
 
     erode_it=1
     dilate_it=1
     altura_y=75
-    offset_y=150
-    boundary=6/7
+    correccion_y=150
+    limites=6/7
     
     #Definición del rango de amarillos
-    lower_yellow = np.array([18, 41, 133])
-    upper_yellow = np.array([30, 255, 255])
+    amarillo_claro = np.array([18, 41, 133])
+    amarillo_oscuro = np.array([30, 255, 255])
     #Definición del rango de blancos 
-    lower_white = np.array([0, 0, 75]) #V más grande menos gris
-    upper_white = np.array([180, 5, 255])
+    blanco_claro = np.array([0, 0, 75]) #V más grande menos gris
+    blanco_oscuro = np.array([180, 5, 255])
 
-    direccion_angulo = -calcula_direccion(cv_img, erode_it, dilate_it, altura_y, offset_y, boundary, lower_yellow, upper_yellow, lower_white, upper_white)*3/4
+    direccion_angulo = -calcula_direccion(cv_img, erode_it, dilate_it, altura_y, correccion_y, limites, amarillo_claro, amarillo_oscuro, blanco_claro, blanco_oscuro)*3/4
     print("stering CV : \n"+str(direccion_angulo))
     # line detector end
 
